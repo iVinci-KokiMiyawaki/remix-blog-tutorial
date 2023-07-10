a104e09a6500b84e879c405b78499e4e99fd6443
iVinci-KokiMiyawaki <kouki.miyawaki@i-vinci.co.jp>
2023-07-11 08:54:43 +0900

    add posts list

--- app/routes/posts._index.tsx
+++ app/routes/posts._index.tsx
@@ -1,5 +1,5 @@
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 
 export const loader = async () => {
   return json({
@@ -22,6 +22,18 @@ export default function Posts() {
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link
+              to={post.slug}
+              className="text-blue-600 underline"
+            >
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
